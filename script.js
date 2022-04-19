52c3ea0dddfc02843cb137e317d3518e42101b37
Prajna1999 <gituprajna20@gmail.com>
2022-04-19 07:07:48 +0530

    showtodo is wired.

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const todoInput=document.querySelector(".task-input input");
 
+let todos= JSON.parse( localStorage.getItem("todoList")) ;
+
+// showTodo function
+
+function showTodo(){
+    todos.forEach((todo, id)=>{
+        console.log(todo, id)
+    })
+}
+
 // add an event listener.
 
 todoInput.addEventListener("keyup", (e)=>{
     let userTask=todoInput.value.trim();
     if(e.key==="Enter" && userTask){
         // save to the localstorage.
-        let todos=localStorage.getItem("todoList");
+        
          
         if(!todos){
             todos=[];
@@ -14,7 +24,8 @@ todoInput.addEventListener("keyup", (e)=>{
             let taskInfo={name: userTask, status:"pending"};
             todos.push(taskInfo);
             localStorage.setItem("todoList",JSON.stringify(todos) );
-        
+            todoInput.value="";
+            showTodo();
         
     }
 
